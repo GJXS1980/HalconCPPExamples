05473dd1a3b043e482f042b79712c286c6de1e16
GJXS1980 <gjxslwz@gmail.com>
2023-08-18 08:21:24 +0800

    update mecheye_object_detection

--- KawasakiDemo/src/mecheye_object_detection.cpp
+++ KawasakiDemo/src/mecheye_object_detection.cpp
@@ -7,18 +7,16 @@
 #include "HDevThread.h"
 #include <opencv2/opencv.hpp>
 
-#include "PclUtil.h"
-#include <pcl/io/ply_io.h>
-
 #include "yolov8.h"
 #include "cmd_line_util.h"
 #include <exception>
 #include <csignal> // 包含信号处理的头文件
+
 bool status_isOK = false;
 
 using namespace HalconCpp;
 
-// SIGINT 信号处理函数(Ctrl+C退出)
+// SIGINT 信号处理函数(Ctrl+C断开相机连接并退出程序)
 void signalHandler(int signum) 
 {
     status_isOK = false;
@@ -35,28 +33,27 @@ void signalHandler(int signum)
 int main(int argc, char *argv[]) 
 {
     mmind::api::MechEyeDevice device;
+
+    mmind::api::ColorMap color;
+    YoloV8Config config;
+    std::string onnxModelPath;
+    std::string colorFile;
+    cv::Mat img;
+
     // 注册信号处理函数
     signal(SIGINT, signalHandler);
-
-    // 创建 VideoWriter 对象
-    cv::VideoWriter videoWriter("output.mp4", cv::VideoWriter::fourcc('H', '2', '6', '4'), 30, cv::Size(1280, 1024));
 
     if (!findAndConnect(device))
     {
         return -1;
     }
 
+    //  相机连接成功
     status_isOK = true;
-    mmind::api::ColorMap color;
-    YoloV8Config config;
-    std::string onnxModelPath;
-    std::string colorFile;
-    
-    cv::Mat img;
-    cv::Mat color8UC3;
-    int count = 0;
+
     try 
     {
+        //  循环采集照片并做实例分割
         while (status_isOK)
         {
             try 
@@ -65,9 +62,6 @@ int main(int argc, char *argv[])
                 showError(device.captureColorMap(color));
                 img = cv::Mat(color.height(), color.width(), CV_8UC3, color.data());
                 colorFile = "boxes.png";
-                // saveMap(color, colorFile);
-                // Read the input image
-                // img = cv::imread(color8UC3);
                 
             }
             catch(const std::exception& e) 
@@ -98,14 +92,13 @@ int main(int argc, char *argv[])
             // Draw the bounding boxes on the image
             yoloV8.drawObjectLabels(img, objects);
 
-            // Save the image to disk
+            // 保存照片
             const auto outputName = colorFile.substr(0, colorFile.find_last_of('.')) + "_annotated.jpg";
 
             //  保存照片
             cv::imwrite(outputName, img);
 
             cv::imshow("实例分割Demo", img);
-            // 等待用户按下按键
             cv::waitKey(1);
  
         }
@@ -114,8 +107,6 @@ int main(int argc, char *argv[])
     {
         std::cerr << "Exception occurred: " << e.what() << std::endl;
     }
-
-    videoWriter.release();
 
     return 0;
 }
